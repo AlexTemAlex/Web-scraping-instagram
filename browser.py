a6080d976d1750effe3d5cee8bf3dfd3a9367654
AlexisCh <temporalxav@outlook.es>
2026-04-18 08:57:32 -0500

    feat: add logic for obtaining Instagram session cookies

--- browser.py
+++ browser.py
@@ -1,19 +1,16 @@
 from playwright.sync_api import sync_playwright
 import os
 
-
 class Browser:
-    def __init__(self, cookies_path=None, headless=False):
+    def __init__(self, playwright, cookies_path=None, headless=False):
+        self.playwright = playwright        # Motor de Playwright
         self.cookies_path = cookies_path    # Ruta opcional donde se guardan/cargan cookies (storage_state)      
         self.headless = headless            # Navegador visible (False) o en segundo plano (True)
-        self.playwright = None              # Motor de Playwright
         self.browser = None                 # Tipo de navegador
         self.context = None
 
     # Inicia Playwright y abre el navegador
     def start(self):
-        self.playwright = sync_playwright().start()         # Inicializa el Motor de Playwright
-
         self.browser = self.playwright.chromium.launch(     # Lanza el navegador Chromium embebido, si se quiere el instalado en host usar channel="chrome",
             headless=self.headless
         )
@@ -26,9 +23,7 @@ class Browser:
         else:   # Si no hay cookies, crea un contexto limpio
             self.context = self.browser.new_context()
 
-        self.page = self.context.new_page()                 # Crea la primera pagina (page)
-
-        return self.page
+        return self.context.new_page()                 # Crea la primera pagina (page)
 
     # Abre una nueva pestaña dentro del mismo contexto, sino crea y devuelve una nueva página
     def new_page(self):
@@ -41,7 +36,3 @@ class Browser:
         # Cierra el navegador si está abierto
         if self.browser:
             self.browser.close()
-
-        # Detiene el motor de Playwright
-        if self.playwright:
-            self.playwright.stop()
