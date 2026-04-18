a9b0930b7fd5e62344ad992c086b502394dde83a
AlexisCh <temporalxav@outlook.es>
2026-04-18 10:51:11 -0500

    feat: add profile upload from Excel

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from browser import Browser
 from playwright.sync_api import sync_playwright
+import pandas as pd
 import time
 import os
 
-storage_path = "storage_instagram.json"
+STORAGE_PATH_INSTAGRAM = "storage_instagram.json"
+STORAGE_PATH = STORAGE_PATH_INSTAGRAM
 
+EXCEL_FILE = "cuentas.xlsx"
+SHEET_NAME = "Hoja1"
+
+ARCHIVO_JSON = "persistencia.json"
 
 def menu():
     print("\n===== SCRAPER MENU =====")
@@ -12,7 +18,11 @@ def menu():
     print("0 Salir")
     return input("Selecciona una opción: ")
 
-def ensure_session(p, storage_path):
+def ensure_session_instagram(p, STORAGE_PATH):
+    """
+    Abre el navegador y crea un archivo de cookies 
+    al iniciar sesión manualmente en Instagram
+    """
     browser = Browser(p, headless=False)
 
     try:
@@ -22,11 +32,28 @@ def ensure_session(p, storage_path):
         input("Inicia sesión manualmente y presiona ENTER...")
 
         # guardar cookies + storage
-        browser.context.storage_state(path=storage_path)
+        browser.context.storage_state(path=STORAGE_PATH)
         print("✔ Cookies guardadas correctamente")
 
     finally:
         browser.close()
+
+def load_excel(file_path, sheet_name=0):
+    """
+    Carga un archivo Excel y devuelve un DataFrame
+    """
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+    return df
+
+def process_profile(page, user_id, url):
+    username = url.rstrip("/").split("/")[-1]
+    print(f"Procesando:\n   ID: {user_id}\n   UserName: {username}\n   URL: {url}")
+
+    try:
+        print(url)
+    except Exception as e:
+        print(f"❌ Error con {url}: {e}")
+
 
 def main():
     with sync_playwright() as p:
@@ -38,20 +65,47 @@ def main():
                 print("Saliendo...")
                 break
             
-            elif opcion == "1":  
+            elif opcion == "1":                  
+                if not os.path.exists(EXCEL_FILE):
+                    print(f"❌ No existe Excel: {EXCEL_FILE}")
+                    return
+                
+                df = load_excel(EXCEL_FILE, SHEET_NAME)
+                df = df[df["url"].fillna("").str.strip() != ""]
+
+                users = df.set_index("id")["url"].to_dict()
+                
+                valid_urls = users.items()
+                
+                total = len(valid_urls)
                 
                 # Valida la existencia de cookies sino las crea mediante iniciar sesion manual
-                if not os.path.exists(storage_path):
-                    ensure_session(p, storage_path)
+                if not os.path.exists(STORAGE_PATH):
+                    ensure_session_instagram(p, STORAGE_PATH)
                 
                 # Iniciar navegador
-                browser = Browser(p, cookies_path=storage_path, headless=False)
-                main_page = browser.start()
+                browser = Browser(p, cookies_path=STORAGE_PATH, headless=False)
+                browser.start_browser()
+                page = browser.new_page()
                 
-                main_page.goto("https://www.instagram.com/")
-                input("\nPresiona ENTER para volver al menú...")
+                page.goto("https://www.instagram.com/")
+                time.sleep(5)
                 
+                print(f"--------------- Iniciando Scrapeo - Total de perfiles {total} ---------------")
+                for idx, (user_id, url) in enumerate(users.items(), start=1):
+                    print(f"# Perfil {idx}/{total}")
+                    
+                    if not url:
+                        continue
+                    
+                    try:
+                        process_profile(page, user_id, url)
+                    except Exception as e:
+                        print(f"❌ Error procesando {url}: {e}")
+
                 browser.close()
+                print("\n✅ Proceso finalizado")
+                print(f"Archivo JSON: {ARCHIVO_JSON}")
 
             else:
                 print("❌ Opción inválida")
